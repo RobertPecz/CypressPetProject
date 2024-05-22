87f1abd3f85446cf25a64477c30093734f58601f
Robert Pecz <rpecz-e@brainloop.com>
2024-05-22 14:28:17 +0200

    Add invalid email test. Fix execution errors.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,6 @@
 import loginData from '../fixtures/login.json';
 import loginPage from '../pages/mainpage.cy.js';
+import errorMessages from "../fixtures/errorMessages.json";
 
 describe('001 Login tests', () => {
   
@@ -15,8 +16,15 @@ describe('001 Login tests', () => {
     const login = new loginPage();
     login.visit();
     const loggedUserPage = login.loginToPage(loginData.loginName, loginData.invalidPassword);
-    login.validateGeneralErrorMessage();
-    login.validateInvalidPasswordError();
+    login.validateErrorMessage(login.elements.generalErrorLabel(), errorMessages.generalErrorMessage)
+    login.validateErrorMessage(login.elements.detailErrorMessageLabel(), errorMessages.invalidPasswordErrorMessage)
   });
 
+  it('03 Login with invalid email and valid password', () => {
+    const login = new loginPage();
+    login.visit();
+    const loggedUserPage = login.loginToPage(loginData.invalidEmail, loginData.password);
+    login.validateErrorMessage(login.elements.generalErrorLabel(), errorMessages.generalErrorMessage)
+    login.validateErrorMessage(login.elements.detailErrorMessageLabel(), errorMessages.autheticationErrorMessage)
+  })
 });
