eb3aee0339bcebaf6e98e7656016be77372ebbcb
Robert Pecz <rpecz-e@brainloop.com>
2024-05-23 15:40:22 +0200

    Add 001-04 Login with valid email and empty password testcase.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -16,15 +16,25 @@ describe('001 Login tests', () => {
     const login = new loginPage();
     login.visit();
     const loggedUserPage = login.loginToPage(loginData.loginName, loginData.invalidPassword);
-    login.validateErrorMessage(login.elements.generalErrorLabel(), errorMessages.generalErrorMessage)
-    login.validateErrorMessage(login.elements.detailErrorMessageLabel(), errorMessages.invalidPasswordErrorMessage)
+    login.validateErrorMessage(login.elements.generalErrorLabel(), errorMessages.generalErrorMessage);
+    login.validateErrorMessage(login.elements.detailErrorMessageLabel(), errorMessages.invalidPasswordErrorMessage);
   });
 
   it('03 Login with invalid email and valid password', () => {
     const login = new loginPage();
     login.visit();
     const loggedUserPage = login.loginToPage(loginData.invalidEmail, loginData.password);
-    login.validateErrorMessage(login.elements.generalErrorLabel(), errorMessages.generalErrorMessage)
-    login.validateErrorMessage(login.elements.detailErrorMessageLabel(), errorMessages.autheticationErrorMessage)
+    login.validateErrorMessage(login.elements.generalErrorLabel(), errorMessages.generalErrorMessage);
+    login.validateErrorMessage(login.elements.detailErrorMessageLabel(), errorMessages.autheticationErrorMessage);
+  })
+
+  it('04 Login with valid email and empty password', () => {
+    const login = new loginPage();
+    login.visit();
+    login.clickOnSignInButton();
+    login.populateUserNameField(loginData.loginName);
+    login.submitLoginButton();
+    login.validateErrorMessage(login.elements.generalErrorLabel(), errorMessages.generalErrorMessage);
+    login.validateErrorMessage(login.elements.detailErrorMessageLabel(), errorMessages.passwordRequiredErrorMessage);
   })
 });
