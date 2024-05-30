468912ffe520e88245c825ba663aac33ac79273e
Robert Pecz <rob.pecz@gmail.com>
2024-05-30 10:11:45 +0200

    [WIP] add register form elements and FillRegistrationForm method.

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,6 +1,14 @@
+import loginPage from '../pages/mainpage.cy.js';
+import registerPage from '../pages/registerPage.cy.js'
+
 describe('001 register tests', () => {
 
     it('05 Register user with valid email and password', () => {
+        const login = new loginPage();
+        login.visit();
+        login.clickOnSignInButton();
+        const register = new registerPage();
+        register.StartRegistration();
         
     })
 })
--- cypress/pages/registerPage.cy.js
+++ cypress/pages/registerPage.cy.js
@@ -1,13 +1,30 @@
 class RegisterPage {
 
+    randomEmailString = "";
+
     elements = {
-        registerEmailAddressInput : () => cy.get('input#email_create'),
-        registerButton : () => cy.get('button#SubmitCreate')
+        registerEmailAddressInput : () => cy.get("input#email_create"),
+        registerButton : () => cy.get("button#SubmitCreate"),
+
+        //Create an account elements
+        mrRadioButton : () => cy.get("input#id_gender1"),
+        mrsRadioButton : () => cy.get("input#id_gender2"),
+        firstNameInput : () => cy.get("input#customer_firstname"),
+        lastNameInput : () => cy.get("input#customer_lastname"),
+        emailInput : () => cy.get("input#email"),
+        passwordInput : () => cy.get("input#passwd"),
+
+        //Date of birth fields
+        dobDayDropdown : () => cy.get("select#days"),
+        dobMonthDropdown : () => cy.get("select#months"),
+        dobYearDropdown : () => cy.get("select#years"),
+        signUpNewsLetterCheckbox : () => cy.get("input#newsletter"),
+        submitRegistrationButton : () => cy.get("button#submitAccount")
     }
 
     CreateRandomString() {
         let result = '';
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
         for (let index = 0; index < 5; index++) {
             result += characters.charAt(Math.floor(Math.random() * charactersLength));           
@@ -18,16 +35,43 @@ class RegisterPage {
 
     CreateRandomEmail() {
         let randomEmail = '';
-        randomEmail += this.CreateRandomString() + '@' + this.CreateRandomString() + '.com'
+        randomEmail += this.CreateRandomString() + "@" + this.CreateRandomString() + ".com"
         
         return randomEmail;
     }
 
     StartRegistration() {
-        this.elements.registerEmailAddressInput().type(this.CreateRandomEmail());
+        this.randomEmailString = this.CreateRandomEmail();
+        this.elements.registerEmailAddressInput().type(this.randomEmailString);
         this.elements.registerButton().click();
 
         return new CreateAccountPage();
+    }
+
+    FillRegistrationForm(gender, firstName, lastName, pwd, dobDay, dobMonth, dobYear) {
+        try{
+            if(gender.toLowerCase() == "male") {
+                this.elements.mrRadioButton().click();
+            }
+            else if(gender.toLowerCase() == "female") {
+                this.elements.mrsRadioButton().click();
+            }
+            else {
+                throw new Error("Only male and female can choose as gender.");
+            }
+        }
+        catch (error) {
+            cy.log(error);
+        }
+
+        this.elements.firstNameInput().type(firstName); 
+        this.elements.lastNameInput().type(lastName);
+        this.elements.emailInput().should("have.text", this.randomEmailString);
+        this.elements.passwordInput().type(pwd)
+        this.elements.dobDayDropdown().select(dobDay);
+        this.elements.dobMonthDropdown().select(dobMonth);
+        this.elements.dobYearDropdown().select(dobYear);
+        this.elements.submitRegistrationButton().click();
     }
 }
 
