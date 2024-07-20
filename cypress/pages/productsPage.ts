2b206b3c980360b2936499954c7c5477980a4510
Robert Pecz <rob.pecz@gmail.com>
2024-07-20 14:22:48 +0200

    [WIP] add products page elements.

--- cypress/pages/productsPage.ts
+++ cypress/pages/productsPage.ts
@@ -1,7 +1,20 @@
 class ProductsPage {
     
     elements = {
-        womenButton : () => cy.get("a[title='Women']"),
-        addToCartButtonIfNotOutOfStock : () => cy.get("span > span.available-dif")
+        womenButton : (Title: string) => cy.get(`a[title='${Title}']`),
+        productAvailableWithDiffOpObject : () => cy.get("span > span.available-dif").parent(),
+        productNoLongerStockLabel : () => cy.get("span#availability_value.label.label-warning"),
+        productInStockLabel : () => cy.get("span#availability_value.label.label-success"),
+        quantityInput : () => cy.get("input#quantity_wanted"),
+        sizeDropDowndown : () => cy.get("select#group_1"),
+        addToCartButton : () => cy.get("button[name='Submit']"),
+        proceedToCheckoutButton : () => cy.get("a[title='Proceed to checkout']"),
+        tosCheckbox : () => cy.get("input#cgv"),
+        bankwirePayButton : () => cy.get("a.bankwire"),
+        chequePayButton : () => cy.get("a.cheque"),
+        confirmMyOrderButton : () => cy.get("button[class='button btn btn-default button-medium'][type='submit']"),
+        orderSuccessLabel : () => cy.get("p.alert.alert-success")
     }
+
+
 }
