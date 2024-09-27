6166e4d3db68b03e6fcefdc7423c1466a5297fee
Robert Pecz <rpecz-e@brainloop.com>
2024-09-27 13:14:25 +0200

    Delete unnecessary bool param. Add register without select dob day testcase.

--- cypress/pages/errorMessages.ts
+++ cypress/pages/errorMessages.ts
@@ -1,6 +1,6 @@
 class ErrorMessage {
     static validateErrorMessage(errorLabel: Cypress.Chainable<JQuery<HTMLElement>>, errorMessage: string) {
-        errorLabel.should("have.text", errorMessage)
+        errorLabel.contains(errorMessage)
     }
 }
 
